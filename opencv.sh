93c5cc6383b51f7f268c3147e0d38852c1db3b7a
lbaitemple <lbai@temple.edu>
2021-03-23 19:24:24 -0400

    Update opencv.sh

--- opencv.sh
+++ opencv.sh
@@ -1,7 +1,9 @@
 sudo apt install python3 python3-setuptools python3-dev python3-venv -y
 pip3 install numpy
 sudo apt-get install build-essential cmake pkg-config -y
-sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev -y
+sudo apt-get install libjpeg-dev libtiff5-dev libjasper-dev  -y
+wget http://ports.ubuntu.com/pool/main/libp/libpng/libpng12-0_1.2.54-1ubuntu1.1_arm64.deb
+sudo dpkg -i libpng12-0_1.2.54-1ubuntu1.1_arm64.deb
 sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev -y
 sudo apt-get install libxvidcore-dev libx264-dev -y
 sudo apt-get install libgtk2.0-dev -y
