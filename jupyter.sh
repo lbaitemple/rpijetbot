06360e13fcd16b9932f82f715755e2b771c60c01
lbaitemple <lbai@temple.edu>
2021-03-24 20:57:58 -0400

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -22,7 +22,7 @@ echo "c.NotebookApp.token = ''" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.password_required = True" >> $HOME/.jupyter/jupyter_lab_config.py
 echo "c.NotebookApp.allow_credentials = False" >> $HOME/.jupyter/jupyter_lab_config.py
 
-cd ~/jetbot/utils
+cd jetbot/utils
 python3 create_jupyter_service.py
 sudo mv jetbot_jupyter.service /etc/systemd/system/jetbot_jupyter.service
 sudo systemctl enable jetbot_jupyter
